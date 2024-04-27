ce6a4a7321830b5b74d02da5f835384520aaa45e
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-04-26 22:56:44 -0400

    Create the Seed File and call it from the Node.js CLI

---
+++ data/seed.js
@@ -0,0 +1,14 @@
+
+function seedDB() {
+    console.log("Desde seedDB")
+}
+
+function clearDB() {
+    console.log("Desde clearDB")
+}
+
+if(process.argv[2] === "--import") {
+    seedDB()
+}else{
+    clearDB()
+}
