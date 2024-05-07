9cfe2c7f3803d902b284502e17cb0616639f2a08
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-06 23:25:51 -0400

    Send test email

--- emails/authEmailService.js
+++ emails/authEmailService.js
@@ -1,5 +1,21 @@
 import { createTransport } from "../config/nodemailer.js"
 
 export async function sendEmailVerification(){
-    console.log("sendEmailVerification")
+    const transporter = createTransport(
+        "sandbox.smtp.mailtrap.io",
+        2525, 
+        "fc14f7f77a9b44",
+        "ffe6ddd8da048f",
+    )
+
+    // Enviar el email
+    const info = await transporter.sendMail({
+        from: 'AppSalon',
+        to: 'correo@correo.com',
+        subject: 'AppSalon - Confirma tu cuenta',
+        text: 'AppSalon - Confirma tu cuenta',
+        html: 'Probando email',
+    })
+
+    console.log('Mensaje Enviado', info.messageId)
 }
