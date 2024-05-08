8abf0168020035faa9230d6fbecdbd4e63f1ca6f
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-07 21:05:13 -0400

    Validate if token exist and confirm account

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { register } from '../controller/authController.js'
+import { register, verifyAccount } from '../controller/authController.js'
 
 const router = express.Router()
 
 // Rutas de autenticacion y registro de usuarios
 
 router.post('/register', register)
+router.get('/verify/:token', verifyAccount)
 
 export default router
