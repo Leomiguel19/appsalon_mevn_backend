2cb5239579010ddc86580fe703817f6b952b50d4
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-12 22:43:17 -0400

    Generate JSON Web Token

--- controller/authController.js
+++ controller/authController.js
@@ -1,5 +1,6 @@
 import User from "../models/User.js"
 import { sendEmailVerification } from "../emails/authEmailService.js"
+import { generateJWT } from "../utils/index.js"
 
 const register = async (req, res) => {
 
@@ -76,8 +77,11 @@ const login = async (req, res) => {
 
     // Comprobar el password
     if(await user.checkPassword(password)){
+
+        const token = generateJWT(user._id)
+        
         res.json({
-            msg: 'Usuario Autenticado'
+            token
         })
     }else{
         const error = new Error('El password es incorrecto')
