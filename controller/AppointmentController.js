986e4709fcc27f856807cf6b2e336812090ef7ec
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-16 21:35:52 -0400

    Define Routing & Appointments Controller

---
+++ controller/AppointmentController.js
@@ -0,0 +1,13 @@
+
+
+const createAppointment = async (req, res) => {
+    try {
+        console.log('Desde createAppointment')
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export {
+    createAppointment
+}
