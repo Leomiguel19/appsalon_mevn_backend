235020391084723aeb7165799704a59e71e7543a
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-16 21:27:55 -0400

    Fix error with the routes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 import { db } from './config/db.js'
 import servicesRoutes from './routes/servicesRoutes.js'
 import authRoutes from './routes/authRoutes.js' 
-import appointmentsRoutes from './routes/appointmentsRoutes.js' 
+import appointmentRoutes from './routes/appointmentRoutes.js' 
 
 // Variables de entorno
 dotenv.config()
@@ -40,7 +40,7 @@ app.use(cors(corsOptions))
 // Definir una ruta
 app.use('/api/services', servicesRoutes)
 app.use('/api/auth', authRoutes)
-app.use('/api/appointments', appointmentsRoutes)
+app.use('/api/appointments', appointmentRoutes)
 
 // Definir puerto
 const PORT = process.env.PORT || 4000
