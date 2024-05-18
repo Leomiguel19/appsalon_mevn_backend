7190e80760b99b2e74a124637ff5c2440b643174
Leonardo Guilarte <leomiguel1907@gmail.com>
2024-05-17 23:27:49 -0400

    Receive data from the frontend

--- controller/AppointmentController.js
+++ controller/AppointmentController.js
@@ -1,8 +1,13 @@
-
+import Appointment from "../models/Appointment.js";
 
 const createAppointment = async (req, res) => {
+    const appointment = req.body
+    appointment.user = req.user._id.toString()
+    console.log(appointment)
+
     try {
-        console.log('Desde createAppointment')
+        const newAppointment = new Appointment(appointment)
+        await newAppointment.save()
     } catch (error) {
         console.log(error)
     }
